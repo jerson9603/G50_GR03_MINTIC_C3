0ac9baf626c437df88cbabf0e09eb1c0f5521aad
jerson9603 <45266480+jerson9603@users.noreply.github.com>
2022-09-08 21:52:21 +0200

    Add files via upload
    Docu, Frontend, Backend y Database actualizados al 08.09.2022

---
+++ 2. backend/src/java/clases/Entrada.java
@@ -0,0 +1,161 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author judit
+ */
+@Entity
+@Table(name = "entradas")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Entrada.findAll", query = "SELECT e FROM Entrada e"),
+    @NamedQuery(name = "Entrada.findByIndexId", query = "SELECT e FROM Entrada e WHERE e.indexId = :indexId"),
+    @NamedQuery(name = "Entrada.findByFechaEntrada", query = "SELECT e FROM Entrada e WHERE e.fechaEntrada = :fechaEntrada"),
+    @NamedQuery(name = "Entrada.findByProveedor", query = "SELECT e FROM Entrada e WHERE e.proveedor = :proveedor"),
+    @NamedQuery(name = "Entrada.findByDetalle", query = "SELECT e FROM Entrada e WHERE e.detalle = :detalle"),
+    @NamedQuery(name = "Entrada.findByCliente", query = "SELECT e FROM Entrada e WHERE e.cliente = :cliente"),
+    @NamedQuery(name = "Entrada.findByPedDevcl", query = "SELECT e FROM Entrada e WHERE e.pedDevcl = :pedDevcl")})
+public class Entrada implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "index_id")
+    private Integer indexId;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "fecha_entrada")
+    @Temporal(TemporalType.DATE)
+    private Date fechaEntrada;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 60)
+    @Column(name = "proveedor")
+    private String proveedor;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 60)
+    @Column(name = "detalle")
+    private String detalle;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 60)
+    @Column(name = "cliente")
+    private String cliente;
+    @Basic(optional = true)
+    @NotNull
+    @Column(name = "ped_devcl")
+    private boolean pedDevcl;
+
+    public Entrada() {
+    }
+
+    public Entrada(Integer indexId) {
+        this.indexId = indexId;
+    }
+
+    public Entrada(Integer indexId, Date fechaEntrada, String proveedor, String detalle, String cliente, boolean pedDevcl) {
+        this.indexId = indexId;
+        this.fechaEntrada = fechaEntrada;
+        this.proveedor = proveedor;
+        this.detalle = detalle;
+        this.cliente = cliente;
+        this.pedDevcl = pedDevcl;
+    }
+
+    public Integer getIndexId() {
+        return indexId;
+    }
+
+    public void setIndexId(Integer indexId) {
+        this.indexId = indexId;
+    }
+
+    public Date getFechaEntrada() {
+        return fechaEntrada;
+    }
+
+    public void setFechaEntrada(Date fechaEntrada) {
+        this.fechaEntrada = fechaEntrada;
+    }
+
+    public String getProveedor() {
+        return proveedor;
+    }
+
+    public void setProveedor(String proveedor) {
+        this.proveedor = proveedor;
+    }
+
+    public String getDetalle() {
+        return detalle;
+    }
+
+    public void setDetalle(String detalle) {
+        this.detalle = detalle;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public boolean getPedDevcl() {
+        return pedDevcl;
+    }
+
+    public void setPedDevcl(boolean pedDevcl) {
+        this.pedDevcl = pedDevcl;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (indexId != null ? indexId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Entrada)) {
+            return false;
+        }
+        Entrada other = (Entrada) object;
+        if ((this.indexId == null && other.indexId != null) || (this.indexId != null && !this.indexId.equals(other.indexId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "clases.Entrada[ indexId=" + indexId + " ]";
+    }
+    
+}
---
+++ 2. backend/src/java/clases/Usuario.java
@@ -0,0 +1,129 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package clases;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import javax.xml.bind.annotation.XmlRootElement;
+
+/**
+ *
+ * @author judit
+ */
+@Entity
+@Table(name = "usuarios")
+@XmlRootElement
+@NamedQueries({
+    @NamedQuery(name = "Usuario.findAll", query = "SELECT u FROM Usuario u"),
+    @NamedQuery(name = "Usuario.findByIndexId", query = "SELECT u FROM Usuario u WHERE u.indexId = :indexId"),
+    @NamedQuery(name = "Usuario.findByNombre", query = "SELECT u FROM Usuario u WHERE u.nombre = :nombre"),
+    @NamedQuery(name = "Usuario.findByNombreUsuario", query = "SELECT u FROM Usuario u WHERE u.nombreUsuario = :nombreUsuario"),
+    @NamedQuery(name = "Usuario.findByContrase\u00f1a", query = "SELECT u FROM Usuario u WHERE u.contrase\u00f1a = :contrase\u00f1a")})
+public class Usuario implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "index_id")
+    private Integer indexId;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 60)
+    @Column(name = "nombre")
+    private String nombre;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 60)
+    @Column(name = "nombre_usuario")
+    private String nombreUsuario;
+    @Basic(optional = false)
+    @NotNull
+    @Size(min = 1, max = 60)
+    @Column(name = "contrase\u00f1a")
+    private String contraseña;
+
+    public Usuario() {
+    }
+
+    public Usuario(Integer indexId) {
+        this.indexId = indexId;
+    }
+
+    public Usuario(Integer indexId, String nombre, String nombreUsuario, String contraseña) {
+        this.indexId = indexId;
+        this.nombre = nombre;
+        this.nombreUsuario = nombreUsuario;
+        this.contraseña = contraseña;
+    }
+
+    public Integer getIndexId() {
+        return indexId;
+    }
+
+    public void setIndexId(Integer indexId) {
+        this.indexId = indexId;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getNombreUsuario() {
+        return nombreUsuario;
+    }
+
+    public void setNombreUsuario(String nombreUsuario) {
+        this.nombreUsuario = nombreUsuario;
+    }
+
+    public String getContraseña() {
+        return contraseña;
+    }
+
+    public void setContraseña(String contraseña) {
+        this.contraseña = contraseña;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (indexId != null ? indexId.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Usuario)) {
+            return false;
+        }
+        Usuario other = (Usuario) object;
+        if ((this.indexId == null && other.indexId != null) || (this.indexId != null && !this.indexId.equals(other.indexId))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "clases.Usuario[ indexId=" + indexId + " ]";
+    }
+    
+}
