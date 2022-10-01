72e65c172e56919f4e05043c4b2700853794911d
SANNI04 <105025750+SANNI04@users.noreply.github.com>
2022-09-30 22:37:22 -0500

    prueba

--- inventario/src/test/java/com/misiontic2022/inventario/InventarioApplicationTests.java
+++ inventario/src/test/java/com/misiontic2022/inventario/InventarioApplicationTests.java
@@ -3,7 +3,7 @@ package com.misiontic2022.inventario;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = InventarioApplicationTests.class)
 class InventarioApplicationTests {
 
 	@Test
